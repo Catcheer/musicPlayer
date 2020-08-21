02463b83fcf7505c35cacfb99407eb799e5ac7f4
zhangchuangye <1072491557@qq.com>
2020-08-21 18:39:09 +0800

    1201

--- src/pages/transfer/components/left.tsx
+++ src/pages/transfer/components/left.tsx
@@ -3,11 +3,12 @@ import React, { useRef } from 'react'
 
 
 function Left() {
+
     let typeList = useRef([
         { text: '团长分享商品', code: '1204' },
         { text: '专门用于直播分享商品', code: '1205' },
         { text: '分享活动页', code: '1300' },
-        { text: '分享商城', code: 122 }
+        { text: '分享商城', code: '1201' }
     ])
 
     return <section>{
